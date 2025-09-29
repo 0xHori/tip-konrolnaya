e3b7c25a6190172ea54014dc6426bb4278f434a6
0xHori <81930932+0xHori@users.noreply.github.com>
2025-09-29 13:59:04 +0300

    Create task2.cpp

---
+++ kr2/var1/task2.cpp
@@ -0,0 +1,44 @@
+//
+// Created by andrey on 29.09.2025.
+// var1
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int m,n;
+    cin>>m>>n;
+    int** arr = new int*[m];
+
+    srand(time(NULL));
+
+    // Выделение памяти
+    for (int i = 0; i < m; i++) {
+        arr[i] = new int[n];
+    }
+
+    // Заполнение случайными числами
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            arr[i][j] = rand() % 10;
+        }
+    }
+
+    // Вывод двумерного массива
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    // Освободить память
+    for (int i = 0; i < m; i++) {
+        delete[] arr[i];
+    }
+    delete[] arr;
+
+    return 0;
+}
