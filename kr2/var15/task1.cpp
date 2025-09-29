2c0ad85a4455f5b6d9f3ce20166f9cf1a6c461f9
0xHori <81930932+0xHori@users.noreply.github.com>
2025-09-29 14:04:04 +0300

    Create task1.cpp

---
+++ kr2/var15/task1.cpp
@@ -0,0 +1,41 @@
+//
+// Created by andrey on 29.09.2025.
+// var15; task1
+//
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russia");
+    srand(time(NULL));
+    
+    int m;
+    int x = 0;
+    cin >> m;
+
+    // Создание динамического массива
+    int* arr = new int[m];
+
+    // Заполнение случайными числами
+    for (int i = 0; i < m; i++) {
+        arr[i] = rand() % 1000;
+        x = x + arr[i] * arr[i];
+    }
+    cout << endl;
+
+    // Вывод чисел, которые находятся в массиве
+    for (int i = 0; i < m; i++) {
+        cout << arr[i] << " ";
+    }
+
+    // Вывод суммы квадратов всех элементов массива
+    cout << x << endl;
+
+    // Освобождение памяти
+    delete[] arr;
+
+    return 0;
+}
